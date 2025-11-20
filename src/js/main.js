70aa35fe036205561bd960123344050bc8934290
Andresmto9 <andres.dev97@gmail.com>
2025-11-19 20:42:43 -0500

    Ajuste ordenmiento archivo main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,6 @@
 const links = document.querySelectorAll('.links a');
-
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        links.forEach(l => l.classList.remove('active'));
-        link.classList.add('active');
-    });
-});
-
 const items = document.querySelectorAll('.timeline-item');
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-}, { threshold: 0.2 });
-
-items.forEach(item => observer.observe(item));
-
 const cards = document.querySelectorAll('.skill-card');
-
-cards.forEach((card, index) => {
-    card.style.opacity = 0;
-    card.style.transform = "translateY(20px)";
-    setTimeout(() => {
-        card.style.transition = "1s";
-        card.style.opacity = 1;
-        card.style.transform = "translateY(0)";
-    }, index * 150);
-});
 
 document.getElementById("contactForm").addEventListener("submit", function(e) {
     e.preventDefault();
@@ -43,4 +14,31 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
     setTimeout(() => {
         msg.textContent = "";
     }, 3000);
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, { threshold: 0.2 });
+
+items.forEach(item => observer.observe(item));
+
+cards.forEach((card, index) => {
+    card.style.opacity = 0;
+    card.style.transform = "translateY(20px)";
+    setTimeout(() => {
+        card.style.transition = "1s";
+        card.style.opacity = 1;
+        card.style.transform = "translateY(0)";
+    }, index * 150);
 });
