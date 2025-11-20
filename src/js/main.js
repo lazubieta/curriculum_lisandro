cc1ab46144744aedd34f9df235cabc700b11de69
Andresmto9 <andres.dev97@gmail.com>
2025-11-19 20:40:18 -0500

    Terminación plantilla curriculum Lisandro.

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+const links = document.querySelectorAll('.links a');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
+
+const items = document.querySelectorAll('.timeline-item');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, { threshold: 0.2 });
+
+items.forEach(item => observer.observe(item));
+
+const cards = document.querySelectorAll('.skill-card');
+
+cards.forEach((card, index) => {
+    card.style.opacity = 0;
+    card.style.transform = "translateY(20px)";
+    setTimeout(() => {
+        card.style.transition = "1s";
+        card.style.opacity = 1;
+        card.style.transform = "translateY(0)";
+    }, index * 150);
+});
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const msg = document.getElementById("formMsg");
+    msg.textContent = "¡Mensaje enviado correctamente!";
+    msg.style.color = "#28a745";
+
+    this.reset();
+
+    setTimeout(() => {
+        msg.textContent = "";
+    }, 3000);
+});
